2657cc5c742dc24e70c92565317d7e623ff349d0
a.abramenko <a.abramenko@yadro.com>
2023-06-11 11:54:56 +0300

    add favicon.ico

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from socket_io import socketio, SomeNamespace
 
 from emitters import emitters_manager
@@ -18,6 +18,11 @@ socketio.on_namespace(SomeNamespace(NAMESPACE))
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/api/rooms')
