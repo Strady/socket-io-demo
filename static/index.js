a9b9d639d55a2dd5cb4bcbb893c44f40e203c4d6
a.abramenko <a.abramenko@yadro.com>
2023-06-11 11:55:28 +0300

    add vue and vuetify

--- static/index.js
+++ static/index.js
@@ -1,14 +1,22 @@
-console.log('HELL WORLD!')
-const socket = io('/some_namespace');
-
-socket.emit(
-    'join',
-    'room 1',
-    messages => {
-      for (const message of messages) {console.log(message)}
-    }
-)
-
-socket.on('some_event', (data) => {
-  console.log('Received message:', data);
+const app = new Vue({
+  el: '#app',
+  delimiters: ['[[', ']]'],
+  vuetify: new Vuetify({theme: {light: true}}),
+  data: {
+    name: 'Strady',
+    job: 'Shitcoder'
+  },
+  created() {
+    const socket = io('/some_namespace');
+    socket.emit(
+        'join',
+        'room 1',
+        messages => {
+          for (const message of messages) {console.log(message)}
+        }
+    )
+    socket.on('some_event', (data) => {
+      console.log('Received message:', data);
+    })
+  }
 })
